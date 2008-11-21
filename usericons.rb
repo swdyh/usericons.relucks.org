701046a3b26bdae2ec0fda144a4edb7ca977a229
swdyh <youhei@gmail.com>
2008-11-21 09:10:17 +0900

    * added MIT-LICENSE  * fixed html

--- usericons.rb
+++ usericons.rb
@@ -64,8 +64,9 @@ use_in_file_templates!
 __END__
 
 @@ index
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html>
+<?xml version="1.0" encoding="UTF-8" ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="ja" xml:lang="ja">
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
   <title>usericons.relucks.org</title>
